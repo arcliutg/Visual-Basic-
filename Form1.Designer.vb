9e9ab5ff1d9f1e069de1204a2453239f19123ad8
浩 刘 <liuhao751678324@163.com>
2023-12-15 23:58:39 +1000

    patch-3

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,30 +24,109 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        TextBox3 = New TextBox()
         Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
         SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Microsoft YaHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        TextBox1.Location = New Point(103, 37)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(377, 46)
+        TextBox1.TabIndex = 0
+        TextBox1.TabStop = False
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Microsoft YaHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        TextBox2.Location = New Point(103, 102)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(377, 46)
+        TextBox2.TabIndex = 1
+        TextBox2.TabStop = False
+        TextBox2.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("温柔养猫人", 13.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Button1.Location = New Point(495, 37)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(121, 111)
+        Button1.TabIndex = 2
+        Button1.Text = "计算总和"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Font = New Font("Microsoft YaHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        TextBox3.Location = New Point(103, 161)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(513, 46)
+        TextBox3.TabIndex = 3
+        TextBox3.TabStop = False
+        TextBox3.TextAlign = HorizontalAlignment.Right
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(53, 24)
+        Label1.Font = New Font("黑体", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label1.Location = New Point(20, 50)
         Label1.Name = "Label1"
-        Label1.Size = New Size(57, 20)
-        Label1.TabIndex = 0
-        Label1.Text = "Label1"
+        Label1.Size = New Size(77, 25)
+        Label1.TabIndex = 4
+        Label1.Text = "变量A"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("黑体", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label2.Location = New Point(20, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 25)
+        Label2.TabIndex = 5
+        Label2.Text = "变量B"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("黑体", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label3.Location = New Point(20, 174)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(77, 25)
+        Label3.TabIndex = 6
+        Label3.Text = "总和F"
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(9.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(9F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(639, 460)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
         Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,39 +2,34 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Show()
         MessageBox.Show("欢迎学习 Visual Basic") '这个会自动蹦出一个弹窗
-        Label1.Text = "欢迎学习 Visual Basic"
+
     End Sub
-    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
         Debug.Print(20 + 10) '使用了 Debug.Print() 方法来输出结果，该方法会在「输出」窗口中显示结果
         Debug.Print(20 - 10)
         Debug.Print(20 * 10)
         Debug.Print(20 / 10)
     End Sub
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
-    Private clickCount As Integer = 0 ' 记录点击次数和显示结果
-    Private Sub Label1_Click(sender As Object, Z As EventArgs) Handles Label1.Click
-
-        clickCount += 1 ' 点击次数加 1
-        Select Case clickCount
-            Case 1
-                Label1.Text = (20 + 10).ToString()
-            Case 2
-                Label1.Text = (20 - 10).ToString()
-            Case 3
-                Label1.Text = (20 * 10).ToString()
-            Case 4
-                Label1.Text = ("最后一次").ToString()
-            Case 5
-                Dim A As String = "我"
-                Dim B As String = "喜"
-                Dim C As String = "欢"
-                Dim D As String = "吃"
-                Dim E As String = "饺子"
-                Label1.Text = A + B + C + D + E
-            Case Else ' 非 1 2 3 4 次点击，令clickCount值为1，重新开始循环
-                clickCount = 1
-                Label1.Text = (20 + 10).ToString()
-        End Select
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+
 End Class
